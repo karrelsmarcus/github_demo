aea9d022bac35bb2f68cc509164edda4fa0ba07b
ngentekis <ngentekis@gmail.com>
2022-11-22 12:21:21 -0600

    updated models, views.

--- project_app/models.py
+++ project_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class supervisor(models.Model):
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
+
+class instructor(models.Model):
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
+
+class course(models.Model):
+    supervisor = models.ForeignKey(supervisor, on_delete=models.CASCADE)
+    instructor = models.ForeignKey(instructor, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    number = models.CharField(max_length=3)
+    section = models.CharField(max_length=3)
--- project_app/views.py
+++ project_app/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import supervisor
+
 
 # Create your views here.
+class login_page(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class landing_page(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class courses_page(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass

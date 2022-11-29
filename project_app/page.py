774d3947ef5f64cf9a75c6456f42d19de0280f57
ngentekis <ngentekis@gmail.com>
2022-11-29 12:37:21 -0600

    updated tests

--- project_app/page.py
+++ project_app/page.py
@@ -1,8 +1,20 @@
 from django.shortcuts import redirect
 
+
 class login_page:
 
     valid_login = False
+
+
+class courses_page:
+    def get_courses(self, user_name):
+        pass
+
+    def create_course(self, course_name, section, number, instructor):
+        pass
+
+    def assign_instructor(self, course_number, instructor_name):
+        pass
 
 
 
---
+++ project_app/unit_tests/courses_page.py
@@ -0,0 +1,11 @@
+from project_app.page import courses_page
+import unittest
+
+class get_courses_tests(unittest.TestCase):
+
+    def setUp(self):
+
+    def test_get_courses(self, user_name):
+
+
+

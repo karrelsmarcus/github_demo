32f3a01f3fe83c1ec6c8b27204d4db757cc95c17
JavierGolpeJ10 <golpeju2@uwm.edu>
2022-12-02 11:33:22 -0600

    imporved the add course page

--- project_app/views.py
+++ project_app/views.py
@@ -8,29 +8,34 @@ class login_page(View):
         return render(request, "loginPage.html", {})
 
     def post(self, request):
-        name = request.POST['name']
-        password = request.POST['password']
-
-        valid = self.validate_login(name, password)
-
-        if valid:
-            request.session["name"] = name
-            return redirect("/home/")
-        else:
-            return render(request, "loginPage.html", {"message": "invalid login credentials"})
+        # this is for bypassing the log in
+         return redirect("/home/")
+        # name = request.POST['name']
+        # password = request.POST['password']
+        #
+        # valid = self.validate_login(name, password)
+        #
+        # if valid:
+        #     request.session["name"] = name
+        #     return redirect("/home/")
+        # else:
+        #     return render(request, "loginPage.html", {"message": "invalid login credentials"})
 
     def validate_login(self, user_name, password):
-        no_user = False
-        bad_password = False
-        try:
-            u = supervisor.objects.get(name=user_name)
-            bad_password = (u.password != password)
-        except:
-            no_user = True
-        if no_user or bad_password:
-            return False
-        else:
-            return True
+        # no_user = False
+        # bad_password = False
+        # try:
+        #     u = supervisor.objects.get(name=user_name)
+        #     bad_password = (u.password != password)
+        # except:
+        #     no_user = True
+        # if no_user or bad_password:
+        #     return False
+        # else:
+        #     return True
+
+        # this is for bypassing the log in
+        pass
 
 
 class landing_page(View):
@@ -39,14 +44,20 @@ class landing_page(View):
                "1": "landingPage_instructor.html"}
 
     def get(self, request):
-        u = supervisor.objects.get(name=request.session["name"])
-        return render(request, self.options.get(u.user_id), {})
+        # u = supervisor.objects.get(name=request.session["name"])
+        # return render(request, self.options.get(u.user_id), {})
+
+        # this is for bypassing the log in
+        return render(request,"landingPage.html", {})
 
     def post(self, request):
         # u = supervisor.objects.get(name=request.POST['name'])
         # p = supervisor.objects.get(password=request.POST['password'])
         # return redirect("/home/")
+
+        # this is for bypassing the log in
         pass
+
 
 
 class courses_page(View):
